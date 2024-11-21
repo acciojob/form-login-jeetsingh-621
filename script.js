da940514e38b7096ab3ae6bd54bcefa6f8b9400b
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-21 23:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	 event.preventDefault();
+	const form = document.getElementById('form1');
+	 const firstName = form.elements['fname'].value;
+  const lastName = form.elements['lname'].value;
+	  alert(`${firstName} ${lastName}`);
 
 }
